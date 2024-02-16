ff5804ce356cedad8340e2e63d591fe327ffa640
Jude Idoko <judidoko@gmail.com>
2024-02-16 16:47:42 +0100

    work on the menu component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,15 @@
+import "./footer.scss";
+import { PiCopyrightThin } from "react-icons/pi";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <span>JD Admin</span>
+      <span>
+        <PiCopyrightThin /> JD Store Admin Dashboard
+      </span>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import "./menu.scss";
+// import { MdHome } from "react-icons/md";
+// import { MdPerson } from "react-icons/md";
+import { menu } from "../../data";
+
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      {menu.map((item) => (
+        <div className="item" key={item.id}>
+          <span className="title">{item.title}</span>
+
+          {item.listItems.map((listItem) => (
+            <Link to="/" className="listItem" key={listItem.id}>
+              <img className="icon" src={listItem.icon} alt={listItem.title} />
+              <span className="listItemTitle">{listItem.title}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Menu;
